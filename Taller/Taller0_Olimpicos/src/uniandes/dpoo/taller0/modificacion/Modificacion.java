d5ec3d3bc37905d2eb6ca54dc70ddf1ea4b8eabc
j-ibanez1 <124117878+j-ibanez1@users.noreply.github.com>
2023-02-02 22:57:30 -0500

    Add files via upload

---
+++ Taller/Taller0_Olimpicos/src/uniandes/dpoo/taller0/modificacion/Modificacion.java
@@ -0,0 +1,21 @@
+package uniandes.dpoo.taller0.modificacion;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modificacion {
+	
+
+	public static void main() throws FileNotFoundException, IOException
+	{
+		
+		
+		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
+	
+	}
+	
+}
